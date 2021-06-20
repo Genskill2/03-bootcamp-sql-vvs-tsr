1e7db9cf897246dc1750289837253dfaec7383a9
vvs-tsr <vvs.tsr@gmail.com>
2021-06-20 12:23:33 +0530

    query 3 trial 3

--- query3.sql
+++ query3.sql
@@ -1,1 +1,1 @@
-select title from books b, subjects s, books_subjects bs where bs.book = b.id and bs.subject = s.id and s.name = 'Technology' or 'Politics';
+select title from books b, subjects s, books_subjects bs where bs.book = b.id and bs.subject = s.id and ( s.name = 'Technology' or s.name ='Politics');
